fc53671ea47f1e16f8e16fc849ce6f55e9c040e1
George Popoff <popoff96@live.com>
2018-03-26 17:02:56 +0300

    Добавил sql схему базы данных и дамп некоторых таблиц

---
+++ database/exercises.dump.sql
@@ -0,0 +1,5 @@
+INSERT INTO `stayfit`.`exercises` (`Difficulty`, `Name`, `Description`, `Image`, `MuscleGroup`, `Price`, `Rating`, `IsBase`) 
+VALUES ('1', 'Standing Dumbbell Curl', 'In a biceps-focused list like this, you can’t leave out the classic curl. So we didn’t. (Don’t worry: The exercises will get more interesting.) But we would ask that you use a weight that makes sense: If you’re swaying back wildly and contorting your body—especially excessively arching your lower back—to lift the load, you should probably get a lighter pair of dumbbells.', 'standing_dumbbell_curl.jpg', '1', '0', '0', '0');
+
+INSERT INTO `stayfit`.`exercises` (`Difficulty`, `Name`, `Description`, `Image`, `MuscleGroup`, `Price`, `Rating`, `IsBase`) 
+VALUES ('3', 'Deadlift', 'Arguably the king of all exercises, the deadlift hits every major muscle group hard, and is perhaps the greatest test of strength there is. Because of all the muscles involved, it releases a huge amount of testosterone (a muscle-building hormone) into the bloodstream. For this reason, the deadlift is a great cornerstone for any fitness plan.', 'deadlift.jpg', '2', '0', '0', '1');
