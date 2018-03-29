e91cbb174d0ba5f6b19ee5f9ecc61c65a82b509e
George Popoff <popoff96@live.com>
2018-03-29 19:29:03 +0300

    Добавил свежий дамп данных

--- database/browser_categories.dump.sql
+++ database/browser_categories.dump.sql
@@ -2,7 +2,7 @@
 -- Query: SELECT * FROM stayfit.exercises
 LIMIT 0, 1000
 
--- Date: 2018-03-29 19:27
+-- Date: 2018-03-29 19:26
 */
 INSERT INTO `exercises` (`id`,`Difficulty`,`Name`,`Description`,`Image`,`MuscleGroup`,`Rating`,`IsBase`,`Voted`) VALUES (1,1,'Standing Dumbbell Curl','In a biceps-focused list like this, you can’t leave out the classic curl. So we didn’t. (Don’t worry: The exercises will get more interesting.) But we would ask that you use a weight that makes sense: If you’re swaying back wildly and contorting your body—especially excessively arching your lower back—to lift the load, you should probably get a lighter pair of dumbbells.','standing_dumbbell_curl.jpg',1,0,0,0);
 INSERT INTO `exercises` (`id`,`Difficulty`,`Name`,`Description`,`Image`,`MuscleGroup`,`Rating`,`IsBase`,`Voted`) VALUES (2,3,'Deadlift','Arguably the king of all exercises, the deadlift hits every major muscle group hard, and is perhaps the greatest test of strength there is. Because of all the muscles involved, it releases a huge amount of testosterone (a muscle-building hormone) into the bloodstream. For this reason, the deadlift is a great cornerstone for any fitness plan.','deadlift.jpg',2,0,1,0);
---
+++ database/browser_categoriy_items.dump.sql
@@ -0,0 +1,21 @@
+/*
+-- Query: SELECT * FROM stayfit.browser_category_items
+LIMIT 0, 1000
+
+-- Date: 2018-03-29 19:28
+*/
+INSERT INTO `browser_category_items` (`Category`,`Item`) VALUES (1,2);
+INSERT INTO `browser_category_items` (`Category`,`Item`) VALUES (1,3);
+INSERT INTO `browser_category_items` (`Category`,`Item`) VALUES (1,4);
+INSERT INTO `browser_category_items` (`Category`,`Item`) VALUES (1,5);
+INSERT INTO `browser_category_items` (`Category`,`Item`) VALUES (1,6);
+INSERT INTO `browser_category_items` (`Category`,`Item`) VALUES (1,1);
+INSERT INTO `browser_category_items` (`Category`,`Item`) VALUES (2,2);
+INSERT INTO `browser_category_items` (`Category`,`Item`) VALUES (2,3);
+INSERT INTO `browser_category_items` (`Category`,`Item`) VALUES (2,4);
+INSERT INTO `browser_category_items` (`Category`,`Item`) VALUES (2,5);
+INSERT INTO `browser_category_items` (`Category`,`Item`) VALUES (2,6);
+INSERT INTO `browser_category_items` (`Category`,`Item`) VALUES (3,3);
+INSERT INTO `browser_category_items` (`Category`,`Item`) VALUES (3,11);
+INSERT INTO `browser_category_items` (`Category`,`Item`) VALUES (3,12);
+INSERT INTO `browser_category_items` (`Category`,`Item`) VALUES (3,13);
